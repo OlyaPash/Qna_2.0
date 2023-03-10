fd058b9a95b120e83626315bbcd7992a536a239a
OlyaPash <olya.pash.rb@gmail.com>
2023-03-10 21:21:00 +0500

    user can add one or many files when creating a answer

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -30,6 +30,16 @@ feature 'User can write the answer on the question page', %{
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'write answer with attached file' do
+      fill_in 'Body', with: 'Text answer'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Create answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'An unauthenticated user cannot create a answer' do
