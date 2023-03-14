8fbfce3e5e726172889d5db5f4e598ea0eda9b68
OlyaPash <olya.pash.rb@gmail.com>
2023-03-14 16:38:49 +0500

    author can delete files: question, answer

--- spec/features/answer/delete_spec.rb
+++ spec/features/answer/delete_spec.rb
@@ -26,4 +26,24 @@ feature 'User can only delete his answer', %{
     expect(page).to_not have_link 'Delete answer'
   end
 
+  describe 'Delete attachment files', js: true do
+    scenario 'Author can delete file to his answer' do
+      answer.files.attach(io: File.open("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
+      answer.files.attach(io: File.open("#{Rails.root}/spec/spec_helper.rb"), filename: 'spec_helper.rb')
+      sign_in(user)
+      visit question_path(question)
+
+      click_on 'Delete file rails_helper.rb'
+      expect(page).to_not have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'No author trying delete files' do
+      answer.files.attach(io: File.open("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
+      sign_in(user_not_author)
+      visit question_path(question)
+
+      expect(page).not_to have_link 'Delete file rails_helper.rb'
+    end
+  end
 end
--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User can only delete his auestion', %{
+feature 'User can only delete his question', %{
   To get a new response from the community
   As an authenticated user
   I would like to be able to delete my question
@@ -23,5 +23,26 @@ feature 'User can only delete his auestion', %{
     visit question_path(question)
 
     expect(page).to_not have_link 'Delete question'
+  end
+
+  describe 'Delete attachment files', js: true do
+    scenario 'Author can delete file to his question' do
+      question.files.attach(io: File.open("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
+      question.files.attach(io: File.open("#{Rails.root}/spec/spec_helper.rb"), filename: 'spec_helper.rb')
+      sign_in(user)
+      visit question_path(question)
+
+      click_on 'Delete file rails_helper.rb'
+      expect(page).to_not have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'No author trying delete files' do
+      question.files.attach(io: File.open("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
+      sign_in(user_not_author)
+      visit question_path(question)
+
+      expect(page).not_to have_link 'Delete file rails_helper.rb'
+    end
   end
 end
