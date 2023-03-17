19c97275ac3ee1770196d2b75599f0f2e1da4f8f
OlyaPash <olya.pash.rb@gmail.com>
2023-03-17 17:34:15 +0500

    2 screencast - polymorphic

---
+++ spec/features/answer/add_links_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can add links to answer', %{
+  In order to provide additional info my answer
+  As an answers author
+  I'd like to be able to add links
+} do
+
+  given(:user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given(:gist_url) { 'https://gist.github.com/OlyaPash/a1998a9c90a501eb807dbae01d60ba32' }
+
+  scenario 'User adds links', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.new-answer' do
+      fill_in 'Body', with: 'Text answer'
+
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+
+      click_on 'Create answer'
+    end
+
+    within '.answers' do
+      expect(page).to have_link 'My gist', href: gist_url
+    end
+  end
+end
