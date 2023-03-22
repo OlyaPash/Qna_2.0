1e4fe1916412aa109637e7e0d6a3f4a3b20f0854
OlyaPash <olya.pash.rb@gmail.com>
2023-03-22 19:26:08 +0500

    gist with spec

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,8 @@ class Link < ApplicationRecord
 
   validates :name, :url, presence: true
   validates :url, format: {with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "are not valid"}
+
+  def gist?
+    url.include?('gist.github.com')
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Link, type: :model do
 
   it { should allow_value('http://example.com', 'https://example.com').for(:url) }
   it { should_not allow_value('fftp://example.com', 'https ://example.com').for(:url) }
+
+  describe 'gist?' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question, user_id: user.id) }
+
+    it 'Link is a gist' do
+      question.links.create(name: 'My Gist', url: 'https://gist.github.com/OlyaPash/a1998a9c90a501eb807dbae01d60ba32')
+
+      expect(question.links.first).to be_gist
+    end
+
+    it 'Link is not a gist' do
+      question.links.create(name: 'Google', url: 'https://www.google.com/')
+
+      expect(question.links.first).to_not be_gist
+    end
+  end
 end
