cb8bbd90f2fc1c0c1f32d38559e06ec86cb96276
OlyaPash <olya.pash.rb@gmail.com>
2023-05-23 14:44:37 +0500

    done screencasts oauth

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'aws-sdk-s3', require: false
 gem 'dotenv-rails'
 gem "cocoon"
 gem 'gon'
+gem 'omniauth'
+gem 'omniauth-github'
+gem 'omniauth-rails_csrf_protection'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :badges
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   def author?(resource)
     id == resource.user_id
@@ -16,5 +18,13 @@ class User < ApplicationRecord
 
   def vote(resource, value)
     votes.create(votable: resource, value: value)
+  end
+
+  def self.find_for_oauth(auth)
+    FindForOauthService.new(auth).call
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
