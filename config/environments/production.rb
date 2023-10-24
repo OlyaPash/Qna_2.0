c0f4453b35b49cbbe08e89af16ffe70124f0709a
OlyaPash <olya.pash.rb@gmail.com>
2023-10-24 15:27:52 +0500

    fix smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: Rails.application.credentials[Rails.env.to_sym][:gmail][:user_name],
-    password: Rails.application.credentials[Rails.env.to_sym][:gmail][:password],
+    address: 'smtp.yandex.ru',
+    port: 465,
+    domain: 'yandex.ru'
+    user_name: Rails.application.credentials[Rails.env.to_sym][:yandex][:user_name],
+    password: Rails.application.credentials[Rails.env.to_sym][:yandex][:password],
     authentication: 'plain',
-    enable_starttls_auto: true }
+    enable_starttls_auto: true },
+    ssl: true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
